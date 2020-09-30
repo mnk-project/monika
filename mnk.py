72d2e603361f4510a2d5eb423576ad1ac3740ff0
Gabriel Kunz <GabrielKunz97@gmail.com>
2020-09-30 17:14:15 -0300

    refactor(mnk): change mnk.py and preprocessor.py

--- mnk.py
+++ mnk.py
@@ -1,1 +1,15 @@
 # pylint: disable=E1101
+
+import numpy as np
+import pandas as pd
+import preprocessor as p
+from tensorflow.keras.models import load_model
+import joblib
+from pathlib import Path as path
+from PIL import Image as img
+import streamlit as st
+
+# Paths definition
+
+
+# Artifacts
